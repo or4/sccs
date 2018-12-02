72717615c6e2259a1b26302938b7dc36ae6ebd3f
vakarevds <vakarevds@yandex-team.ru>
2018-12-02 23:33:50 +0700

    some refactor

--- src/sccs/other/pg-utils.ts
+++ src/sccs/other/pg-utils.ts
@@ -7,41 +7,41 @@ const client = new pg.Client({
   database: 'edu',
   password: '1',
   port: 5432,
-})
-client.connect()
+});
+client.connect();
 
 
 const query = async (text: string) => {
   return new Promise((resolve, reject) => {
     try {
       client.query(text, (err, res) => {
-        if (err) { reject(err); }
-        else { resolve(res); }
-      })
+        if (err) { reject(err) }
+        else { resolve(res) }
+      });
     } catch (err) {
       // reject(err);
     }
-  })
-}
+  });
+};
 
 
 
 export type Tables = 'stack';
 
 export const clearTable = async (table: Tables) => {
-  return await query(`delete from sccs.${table} where 1=1`);
-}
+  return query(`delete from sccs.${table} where 1=1`);
+};
 export const addRow = async (table: Tables, values: object) => {
   const preparedKeys = R.pipe(R.keys, R.join(','))(values);
   const preparedValues = R.pipe(R.values, R.join(','))(values);
   const sql = `insert into sccs.${table} (${preparedKeys}) values (${preparedValues})`;
-  return await query(sql);
-}
+  return query(sql);
+};
 export const select = async (table: Tables) => {
   const sql = `select * from sccs.${table}`;
   const rows = await query(sql);
   return rows && (rows as any).rows || [];
-}
+};
 
 
 // const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
